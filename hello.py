375054dc189624c3d73313611042f994624e98cb
snehashastry <sneha.shastry10@gmail.com>
2026-06-24 23:44:24 +0530

    Add sum, factorial, and Fibonacci calculations

--- hello.py
+++ hello.py
@@ -11,3 +11,39 @@ a, b = b, a
 print("After swapping:")
 print("First number:", a)
 print("Second number:", b)
+
+---------------------------------------------------------------------------------------------------------
+import sys
+
+a = int(sys.argv[1])
+b = int(sys.argv[2])
+
+print("Sum =", a + b)
+
+-----------------------------------------------------------------------------------------------------------
+
+import sys
+
+n = int(sys.argv[1])
+
+fact = 1
+
+for i in range(1, n + 1):
+fact *= i
+
+print("Factorial =", fact)
+
+--------------------------------------------------------------------------------------------------------------
+
+import sys
+
+n = int(sys.argv[1])
+
+a = 0
+b = 1
+
+print("Fibonacci Series:")
+
+for i in range(n):
+print(a, end=" ")
+a, b = b, a + b
